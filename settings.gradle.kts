e770195b43594e818f8e52d60bfabe2a7eb17bd7
Sergey <sergeypanz355@mail.ru>
2023-10-20 23:02:47 +0700

    Проект создан

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PR22_AubovSergey_PR-21.102"
+include(":app")
