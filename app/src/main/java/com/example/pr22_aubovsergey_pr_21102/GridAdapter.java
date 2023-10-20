17cf8307a2af58f8f92aae1dd45505639b4e1fd7
Sergey <sergeypanz355@mail.ru>
2023-10-20 23:38:32 +0700

    Генерация картинок готова

--- app/src/main/java/com/example/pr22_aubovsergey_pr_21102/GridAdapter.java
+++ app/src/main/java/com/example/pr22_aubovsergey_pr_21102/GridAdapter.java
@@ -1,26 +1,54 @@
 package com.example.pr22_aubovsergey_pr_21102;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 class GridAdapter extends BaseAdapter
 {
     private Context mContext;
     private Integer mCols, mRows;
+    private ArrayList<String> arrPict; // массив картинок
+    private String PictureCollection; // Префикс набора картинок
+    private Resources mRes; // Ресурсы приложени
 
     public GridAdapter(Context context, int cols, int rows)
     {
         mContext = context;
         mCols = cols;
         mRows = rows;
+        arrPict = new ArrayList<String>();
+        // Пока определяем префикс так, позже он будет браться из настроек
+        PictureCollection = "animal";
+        // Получаем все ресурсы приложения
+        mRes = mContext.getResources();
+
+        // Метод заполняющий массив vecPict
+        makePictArray ();
+    }
+
+    private void makePictArray () {
+        // очищаем вектор
+        arrPict.clear();
+        // добавляем
+        for (int i = 0; i < ((mCols * mRows) / 2); i++)
+        {
+            arrPict.add (PictureCollection + Integer.toString (i));
+            arrPict.add (PictureCollection + Integer.toString (i));
+        }
+        // перемешиваем
+        Collections.shuffle(arrPict);
     }
 
     @Override
     public int getCount() {
-        return mCols * mRows;
+        return mCols*mRows;
     }
 
     @Override
@@ -42,10 +70,12 @@ class GridAdapter extends BaseAdapter
             view = new ImageView(mContext);
         else
             view = (ImageView)convertView;
-
-        view.setImageResource(R.drawable.animal7);
-
+        // Получаем идентификатор ресурса для картинки,
+        // которая находится в векторе vecPict на позиции position
+        Integer drawableId = mRes.getIdentifier(arrPict.get(position), "drawable", mContext.getPackageName());
+        view.setImageResource(drawableId);
         return view;
     }
 }
+
 
